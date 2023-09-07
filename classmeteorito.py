50603c474e9c079593bb8c20a1d1b8278e0c1ec8
Kevin Restrepo <ksantyr@gmail.com>
2023-09-06 20:50:34 -0500

    Meteoritos implementados

--- classmeteorito.py
+++ classmeteorito.py
@@ -7,15 +7,13 @@ class Meteorito:
         self.imagen = pygame.image.load('Imagenes/meteorito.png') #imagen del meteorito
         self.pos_x = random.randint(100,700) # posición en x en la ventana
         self.pos_y = 0 # posición en y en la ventana
-        self.velocidad = 3 # velocidad
+        self.velocidad = 1.5 # velocidad
         self.rect = self.imagen.get_rect() #se rellena la imagen como un rectangulo con el fin de gestionar las colisiones
 
     def actualizacion_pos(self,ventana):
-        
-        
         ventana.blit(self.imagen,[self.pos_x,self.pos_y])# proyección de la imagen
-        pygame.display.flip() # actualización de la ventana 
+        #pygame.display.flip() # actualización de la ventana 
         self.pos_y += self.velocidad # actualizacion de la posición
     
     def __del__(self): # destructor
-        self.y = 900
+        print('Objeto eliminado')
--- main.py
+++ main.py
@@ -2,23 +2,35 @@ import pygame
 import sys
 import classmeteorito
 
-
 pygame.init() # inicializamos la libreria de pygame
-tamaño = (1000,800)  # tamaño de la ventana
-meteorito = classmeteorito.Meteorito()
+tamaño = (1400,800)  # tamaño de la ventana
 ruta_fondo = 'Imagenes/espacio3.jpeg' #ruta de la imagen de fondo
+reloj = pygame.time.Clock()
 
 ventana = pygame.display.set_mode(tamaño) # creación de la ventana
+ventana.fill((255,255,255)) # color blanco en la ventana
 fondo = pygame.image.load(ruta_fondo).convert() # imagen de fondo del juego
 
-while True:
+meteoritos = [classmeteorito.Meteorito()] #lista con todos los metehoritos generados
 
-    ventana.blit(fondo,[0,0]) # proyección de la imagen
-    meteorito.actualizacion_pos(ventana)
+# Configuración del temporizador para generar objetos
+GENERATE_OBJECT_EVENT = pygame.USEREVENT + 1  # Evento personalizado
+pygame.time.set_timer(GENERATE_OBJECT_EVENT, 1300)  # Generar cada 1.3 segundos (1300 milisegundos)
+
+while True:
+    ventana.blit(fondo,[0,0]) # proyección de la imagen de fondo     
     for evento in pygame.event.get(): # registro de eventos dentro de la ventana
         if evento.type == pygame.QUIT: # cerrar la ventana
             sys.exit()
-    
-    pygame.display.flip() # actualización de la pantalla
+        elif evento.type == GENERATE_OBJECT_EVENT: # registro de la generación de las monedas
+            meteoritos.append(classmeteorito.Meteorito()) # generamos un meteorito
 
+    for i in meteoritos: # actualizamos la posicón de los meteorito
+        i.actualizacion_pos(ventana)
+
+    pygame.display.flip() # actualización de la pantalla
+    reloj.tick(250)
+
+for i in meteoritos: # destrucción de objetos
+    del i
 pygame.quit()
